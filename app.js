e54dcca68cf13c8df812956bfff9ca5ebc41a89b
aaronmorgenegg <aaronmorgenegg@gmail.com>
2016-12-14 22:46:48 -0700

    figured out inserting into database

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var ejs = require('ejs');
 var path = require('path');
+var mysql = require('mysql');
+
+var dbHost = process.env.IP || 'localhost',
+  dbUser = process.env.C9_USER || 'root';
+
+var connection = mysql.createConnection({
+  host : dbHost,
+  user : dbUser,
+  password : '',
+  database : 'highscores'
+});
 
 var app = express();
 
@@ -11,9 +22,38 @@ app.use(express.static(__dirname + "/../public"));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.render('index.ejs');
+  //need to get current list of names and scores from the database, pass that into the website in order to show the high scores
+  var usernames = [
+        { name: 'test1' },
+        { name: 'test2' },
+        { name: 'test3' }
+    ];
+  var finalScores = [
+        { score: 1 },
+        { score: 2 },
+        { score: 3 }
+    ];
+  res.render('index.ejs', {
+        usernames: usernames,
+        finalScores: finalScores
+    });
+  //need to get name and score data from the website in order to insert it into the database
+  var setFlds = {
+    name: username,
+    score: finalScore
+  };
+
+  var sql = 'insert into `users` set ?';
+
+  connection.query(sql, [setFlds], function(err,result){
+    //error handling
+    if (err) {
+      console.error('Error inserting' + err.stack);
+    }
+    else {
+      console.log("Success");
+    }
+  });
 });
 
-app.listen(3000, function(){
-  console.log("The server is running on localhost:3000")
-})
+app.listen(process.env.PORT, process.env.IP);
